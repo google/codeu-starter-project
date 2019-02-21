289abf713d508964c893a4a27100fdc26da81875
Mario Verdugo <mv7757@email.arizona.edu>
2019-02-21 09:22:25 -0700

    added the redirect to include recipient.

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -81,6 +81,6 @@ public class MessageServlet extends HttpServlet {
     Message message = new Message(user, text, recipient);
     datastore.storeMessage(message);
 
-    response.sendRedirect("/user-page.html?user=" + user);
+    response.sendRedirect("/user-page.html?user=" + recipient);
   }
 }
