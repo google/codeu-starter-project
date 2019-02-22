43ee101405b4d4b44ccd1da33779d8f07d6213e6
Chen Meng <cmeng@codeustudents.com>
2019-02-22 10:24:35 -0800

    Add logic to get all messages under DataStore

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -79,4 +79,38 @@ public class Datastore {
 
     return messages;
   }
-}
+
+  /**
+   * Gets all messages posted by a every user.
+   *
+   * @return a list of all the messages, or empty list if no one has ever posted a
+   *     message. List is sorted by time descending.
+   */
+  public List<Message> getAllMessages(){
+    List<Message> messages = new ArrayList<>();
+
+    Query query = new Query("Message")
+      .addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+
+        Message message = new Message(id, user, text, timestamp);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
+    }
+
+    return messages;
+  }
+
+} // End of class
