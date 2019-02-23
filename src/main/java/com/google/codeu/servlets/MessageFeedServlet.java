6025e6a2da8c5518603e6a8f12b89344685284f3
Chen Meng <cmeng@codeustudents.com>
2019-02-23 12:37:41 -0800

    Add message fetching logic

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -17,11 +17,16 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import com.google.gson.Gson;
 
 /**
  * Handles fetching all messages for the public feed.
@@ -29,10 +34,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/feed")
 public class MessageFeedServlet extends HttpServlet {
   
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+ 
+  /**
+   * Responds with a JSON representation of Message data for all users.
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
+
+    response.setContentType("application/json");
   
-    response.getOutputStream().println("this will be my message feed");
+    List<Message> messages = datastore.getMessages("all");
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
+  
+    response.getOutputStream().println(json);
   }
 }
