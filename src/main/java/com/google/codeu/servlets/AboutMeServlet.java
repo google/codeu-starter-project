1abc5659e660d9bc0d21a817660ebc9638b997b0
Rebecca Xu <rxu2460@gmail.com>
2019-02-23 01:43:44 -0800

    spacing

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -1,14 +1,14 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
-
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.codeu.data.Datastore;
+
 
 /**
  * Handles fetching and saving user data.
@@ -26,34 +26,34 @@ public class AboutMeServlet extends HttpServlet {
  /**
   * Responds with the "about me" section for a particular user.
   */
- @Override
+   @Override
  public void doGet(HttpServletRequest request, HttpServletResponse response)
- throws IOException {
-     response.setContentType("text/html");
-     String user = request.getParameter("user");
-     if(user == null || user.equals("")) {
-         return;
-     }
+      throws IOException {
+    response.setContentType("text/html");
+    String user = request.getParameter("user");
+    if(user == null || user.equals("")) {
+      return;
+    }
   
-  String aboutMe = "This is " + user + "'s about me.";
+    String aboutMe = "This is " + user + "'s about me.";
   
-  response.getOutputStream().println(aboutMe);
+    response.getOutputStream().println(aboutMe);
   }
  
- @Override
+  @Override
  public void doPost(HttpServletRequest request, HttpServletResponse response)
- throws IOException {
-     UserService userService = UserServiceFactory.getUserService();  
-     if (!userService.isUserLoggedIn()) {
-         response.sendRedirect("/index.html");
-         return;
-     }
+     throws IOException {
+    UserService userService = UserServiceFactory.getUserService();  
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
   
-  String userEmail = userService.getCurrentUser().getEmail();
+    String userEmail = userService.getCurrentUser().getEmail();
 
-  System.out.println("Saving about me for " + userEmail);
+    System.out.println("Saving about me for " + userEmail);
   // TODO: save the data
   
-  response.sendRedirect("/user-page.html?user=" + userEmail);
- }
+    response.sendRedirect("/user-page.html?user=" + userEmail);
+  }
 }
