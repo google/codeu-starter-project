938b6dcc9fcead1c2754e2034e0ccb3e8d44f9ef
Faesal Murad <faesalm@uvic.ca>
2019-02-24 15:34:21 -0800

    fixed format

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -84,26 +84,26 @@ public class Datastore {
    *
    * @return a list of results, or empty list if no results found
    */
-  public List<Message> fetchMessages (Query query){
-	PreparedQuery results = datastore.prepare(query);
-	List<Message> messages = new ArrayList<>();
+  public List<Message> fetchMessages(Query query) {
+    PreparedQuery results = datastore.prepare(query);
+    List<Message> messages = new ArrayList<>();
     for (Entity entity : results.asIterable()) {
-        try {
-          String idString = entity.getKey().getName();
-          UUID id = UUID.fromString(idString);
-          String user = (String) entity.getProperty("user");
-          String text = (String) entity.getProperty("text");
-          long timestamp = (long) entity.getProperty("timestamp");
-          String recipient = (String) entity.getProperty("recipient");
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+        String recipient = (String) entity.getProperty("recipient");
 
-          Message message = new Message(id, user, text, timestamp, recipient);
-          messages.add(message);
-        } catch (Exception e) {
-          System.err.println("Error reading message.");
-          System.err.println(entity.toString());
-          e.printStackTrace();
-        }
+        Message message = new Message(id, user, text, timestamp, recipient);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
       }
+    }
     return messages;
   }
   
