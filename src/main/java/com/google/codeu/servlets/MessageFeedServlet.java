1d90f1c6651a9c538e8f76a66295b105787d33a4
Faesal Murad <faesalm@uvic.ca>
2019-02-24 15:43:59 -0800

    trying to fix format

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -3,41 +3,40 @@ package com.google.codeu.servlets;
 import java.io.IOException;
 import java.util.List;
 
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import com.google.gson.Gson;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Message;
-import com.google.gson.Gson;
-
 /**
  * Handles fetching all messages for the public feed.
  */
 @WebServlet("/feed")
-public class MessageFeedServlet extends HttpServlet{
-  
- private Datastore datastore;
+public class MessageFeedServlet extends HttpServlet {
 
- @Override
- public void init() {
-  datastore = new Datastore();
- }
- 
- /**
-  * Responds with a JSON representation of Message data for all users.
-  */
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-   throws IOException {
+  private Datastore datastore;
 
-  response.setContentType("application/json");
-  
-  List<Message> messages = datastore.getAllMessages();
-  Gson gson = new Gson();
-  String json = gson.toJson(messages);
-  
-  response.getOutputStream().println(json);
- }
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  /**
+   * Responds with a JSON representation of Message data for all users.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    response.setContentType("application/json");
+
+    List<Message> messages = datastore.getAllMessages();
+    Gson gson = new Gson();
+    String json = gson.toJson(messages);
+
+    response.getOutputStream().println(json);
+  }
 }
