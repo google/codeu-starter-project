b1f4f9927eca7fc83c4e2fc3c22fcd7303985ec6
Faesal Murad <faesalm@uvic.ca>
2019-02-23 22:26:52 -0800

    added servlet logic for message feed

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -1,22 +1,43 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.Message;
+import com.google.gson.Gson;
+
 /**
  * Handles fetching all messages for the public feed.
  */
 @WebServlet("/feed")
-public class MessageFeedServlet extends HttpServlet {
+public class MessageFeedServlet extends HttpServlet{
   
-  @Override
+ private Datastore datastore;
+
+ @Override
+ public void init() {
+  datastore = new Datastore();
+ }
+ 
+ /**
+  * Responds with a JSON representation of Message data for all users.
+  */
+ @Override
  public void doGet(HttpServletRequest request, HttpServletResponse response)
-     throws IOException { 
-    // This message is hardcoded for now just to test. Will change later
-    response.getOutputStream().println("this will be my message feed");
-  }
+   throws IOException {
+
+  response.setContentType("application/json");
+  
+  List<Message> messages = datastore.getAllMessages();
+  Gson gson = new Gson();
+  String json = gson.toJson(messages);
+  
+  response.getOutputStream().println(json);
+ }
 }
