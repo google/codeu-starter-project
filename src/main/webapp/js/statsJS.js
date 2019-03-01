5b2f699b776df090bbf209627dfa143450efee29
hmc-cod-f18 <csimon@cs.hmc.edu>
2019-03-01 09:26:07 -0800

    Added functional Stats Page

---
+++ src/main/webapp/js/statsJS.js
@@ -0,0 +1,24 @@
+   // Fetch stats and display them in the page.
+    function fetchStats(){
+      const url = '/stats';
+      fetch(url).then((response) => {
+        return response.json();
+      }).then((stats) => {
+        const statsContainer = document.getElementById('stats-container');
+        statsContainer.innerHTML = '';
+
+        const messageCountElement = buildStatElement('Message count: ' + stats.messageCount);
+        statsContainer.appendChild(messageCountElement);
+      });
+    }
+
+    function buildStatElement(statString){
+     const statElement = document.createElement('p');
+     statElement.appendChild(document.createTextNode(statString));
+     return statElement;
+    }
+
+    // Fetch data and populate the UI of the page.
+    function buildUI(){
+     fetchStats();
+    }
