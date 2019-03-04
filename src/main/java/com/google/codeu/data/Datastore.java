b924d3e1bc3e2ad5e827ace5fd3aa17c0de2b9aa
Alan <Alan@192.168.1.189>
2019-03-03 16:23:02 -0800

    Added functionality to post messages on any users page

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -49,27 +49,28 @@ public class Datastore {
   }
 
   /**
-   * Gets messages posted by a specific user.
+   * Gets messages sent to a specific recipient.
    *
-   * @return a list of messages posted by the user, or empty list if user has never posted a
-   *     message. List is sorted by time descending.
+   * @return a list of messages sent to the recipient, or empty list if recipient
+   *     has never received has never posted a
+   *     List is sorted by time descending.
    */
-  public List<Message> getMessages(String user) {
+  public List<Message> getMessages(String recipient) {
     List<Message> messages = new ArrayList<>();
 
     Query query =
         new Query("Message")
-            .setFilter(new Query.FilterPredicate("user", FilterOperator.EQUAL, user))
+            .setFilter(new Query.FilterPredicate("recipient", FilterOperator.EQUAL, recipient))
             .addSort("timestamp", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
       try {
         String idString = entity.getKey().getName();
-        UUID id = UUID.fromString(idString);
-        String text = (String) entity.getProperty("text");
+        UUID id = UUID.fromString(idString);    
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");              
         long timestamp = (long) entity.getProperty("timestamp");
-        String recipient = (String) entity.getProperty("recipient");
 
         Message message = new Message(id, user, text, timestamp, recipient);
         messages.add(message);
@@ -79,7 +80,6 @@ public class Datastore {
         e.printStackTrace();
       }
     }
-
     return messages;
   }
   
