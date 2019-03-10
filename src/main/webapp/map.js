9603a12c7d07218a2884e8b5e782afa10b5d0651
Alyssa Lau <alyssalau@berkeley.edu>
2019-03-10 15:44:03 -0700

    separated html, css, js into separate files for the map, added feature to display locations and markers and feature to display current user location using geolocation API

--- src/main/webapp/map.js
+++ src/main/webapp/map.js
@@ -5,7 +5,6 @@ function createMap(){
     center: {lat: 37.422403, lng: -122.088073},
     zoom: 15
   });
-
   addLandmark(map, 37.423829, -122.092154, 'Google West Campus', 'Google West Campus is home to YouTube and Maps.')
   addLandmark(map, 37.421903, -122.084674, 'Stan the T-Rex', 'This is Stan, the T-Rex statue.')
   addLandmark(map, 37.420919, -122.086619, 'Permanente Creek Trail', 'Permanente Creek Trail connects Google to a system of bike trails.');
