8c478419a93c3bbeb4115936c011b21ca9288e77
hmc-cod-f18 <csimon@cs.hmc.edu>
2019-03-09 20:15:12 -0800

    Sentiment score shows up on messages now

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -77,7 +77,8 @@ function buildMessageDiv(message) {
   const headerDiv = document.createElement('div');
   headerDiv.classList.add('message-header');
   headerDiv.appendChild(document.createTextNode(
-      message.user + ' - ' + new Date(message.timestamp)));
+      message.user + ' - ' + new Date(message.timestamp) + 
+      ' [' + message.sentimentScore + ']'));
 
   const bodyDiv = document.createElement('div');
   bodyDiv.classList.add('message-body');
