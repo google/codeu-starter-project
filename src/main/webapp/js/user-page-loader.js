6c94460ce830ff6f1cd9ce459a9828d1b084cb73
Mario Verdugo <mv7757@email.arizona.edu>
2019-03-18 14:19:32 -0700

    added in the recipient to the fetchImageUploadUrlAndShowForm function

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -41,24 +41,21 @@ function showMessageFormIfLoggedIn() {
         if (loginStatus.isLoggedIn &&
             loginStatus.username == parameterUsername) {
           const messageForm = document.getElementById('message-form');
-          //messageForm.action = '/messages?recipient=' + parameterUsername;
           fetchImageUploadUrlAndShowForm();
-          //messageForm.classList.remove('hidden');
         }
       });
-  //document.getElementById('about-me-form').classList.remove('hidden');
+  document.getElementById('about-me-form').classList.remove('hidden');
 
 }
 
 function fetchImageUploadUrlAndShowForm() {
   fetch('/image-upload-url?recipient=' + parameterUsername).then((response) => {
-        console.log("heelo in fetchImageUploadUrlAndShowForm")
+        //console.log(response.text());
         return response.text();
       })
       .then((imageUploadUrl) => {
         const messageForm = document.getElementById('message-form');
         messageForm.action = imageUploadUrl;
-        console.log(imageUploadUrl);
         messageForm.classList.remove('hidden');
       });
 }
