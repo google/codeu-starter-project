c4b916c3ecd3682f7cd91f8e227b66f17b25927a
Faesal Murad <faesalm@uvic.ca>
2019-03-19 19:43:30 -0700

    prettier

--- src/main/webapp/js/ufo_map_loader.js
+++ src/main/webapp/js/ufo_map_loader.js
@@ -1,18 +1,19 @@
-function createUfoSightingsMap(){
-      fetch('/ufo-data').then(function(response) {
-        return response.json();
-      }).then((ufoSightings) => {
-          
-        const map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 35.78613674, lng: -119.4491591},
-          zoom:7
-        });
+function createUfoSightingsMap() {
+  fetch("/ufo-data")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(ufoSightings => {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 35.78613674, lng: -119.4491591 },
+        zoom: 7
+      });
 
-        ufoSightings.forEach((ufoSighting) => {
-          new google.maps.Marker({
-            position: {lat: ufoSighting.lat, lng: ufoSighting.lng},
-            map: map
-          });  
+      ufoSightings.forEach(ufoSighting => {
+        new google.maps.Marker({
+          position: { lat: ufoSighting.lat, lng: ufoSighting.lng },
+          map: map
         });
       });
+    });
 }
