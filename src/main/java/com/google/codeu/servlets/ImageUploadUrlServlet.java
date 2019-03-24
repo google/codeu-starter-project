54a4ade20b2733d9e5225a2ccbda19620cd308a9
Alan <Alan@eulalias-air.domain>
2019-03-23 22:01:55 -0700

    Created servlet to allow access to a URL so users can upload images

---
+++ src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
@@ -0,0 +1,26 @@
+package com.google.codeu.servlets;
+
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Provides access to a URL that allows a user to upload an image to Blobstore.
+ */
+@WebServlet("/image-upload-url")
+public class ImageUploadUrlServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    String uploadUrl = blobstoreService.createUploadUrl("/messages") ;
+
+    response.setContentType("text/html");
+    response.getOutputStream().println(uploadUrl);
+  }
+}
