8e2685096bc7c3c8fd6711f6f9f69ae45843fdde
tiananguyen <tiananguyen99@gmail.com>
2019-03-24 22:15:17 -0700

    Add function to retrieve markers

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -121,4 +121,37 @@ public class Datastore {
     PreparedQuery results = datastore.prepare(query);
     return results.countEntities(FetchOptions.Builder.withLimit(1000));
   }
+
+  /** Retrieves markers for maps. */
+  public List<UserMarker> getMarkers() {
+    List<UserMarker> markers = new ArrayList<>();
+
+    Query query = new Query("UserMarker");
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+     try {
+      double lat = (double) entity.getProperty("lat");
+      double lng = (double) entity.getProperty("lng");
+      String content = (String) entity.getProperty("content");
+
+      UserMarker marker = new UserMarker(lat, lng, content);
+      markers.add(marker);
+     } catch (Exception e) {
+      System.err.println("Error reading marker.");
+      System.err.println(entity.toString());
+      e.printStackTrace();
+     }
+    }
+    return markers;
+  }
+
+  /** Stores markers for maps. */
+  public void storeMarker(UserMarker marker) {
+    Entity markerEntity = new Entity("UserMarker");
+    markerEntity.setProperty("lat", marker.getLat());
+    markerEntity.setProperty("lng", marker.getLng());
+    markerEntity.setProperty("content", marker.getContent());
+    datastore.put(markerEntity);
+  }
 }
