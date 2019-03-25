b6e3a87552e53d4cf2564fd95c5d2be0e7b258de
Alan Ensastegui <Alan@Eulalias-Air.domain>
2019-03-25 02:13:57 -0700

    prettier

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -107,9 +107,11 @@ function buildMessageDiv(message) {
 
 function fetchAboutMe(){
   const url = '/about?user=' + parameterUsername;
+
   fetch(url).then(response => {
     return response.text();
-  }).then(aboutMe => {
+  })
+  .then(aboutMe => {
     const aboutMeContainer = document.getElementById('about-me-container');
     if(aboutMe == ''){
       aboutMe = 'This user has not entered any information yet.';
