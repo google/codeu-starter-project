3813d7a3e539f1f60ccfbd0926ca141bde253594
Alan Ensastegui <Alan@Eulalias-Air.domain>
2019-03-25 02:16:23 -0700

    prettier

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -109,18 +109,18 @@ function fetchAboutMe(){
   const url = '/about?user=' + parameterUsername;
 
   fetch(url)
-  .then(response => {
-    return response.text();
-  })
-  .then(aboutMe => {
-    const aboutMeContainer = document.getElementById('about-me-container');
-    if(aboutMe == ''){
-      aboutMe = 'This user has not entered any information yet.';
-    }
+    .then(response => {
+      return response.text();
+    })
+    .then(aboutMe => {
+      const aboutMeContainer = document.getElementById('about-me-container');
+      if(aboutMe == ''){
+        aboutMe = 'This user has not entered any information yet.';
+      }
 
-    aboutMeContainer.innerHTML = aboutMe;
+      aboutMeContainer.innerHTML = aboutMe;
 
-  });
+    });
 }
 
 /** Fetches data and populates the UI of the page. */
