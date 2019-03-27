74df895b95d77e419fa29a81e443ae21506d48a5
tiananguyen <tiananguyen99@gmail.com>
2019-03-26 22:24:54 -0400

    Fix order

--- src/main/java/com/google/codeu/servlets/UserMarkerServlet.java
+++ src/main/java/com/google/codeu/servlets/UserMarkerServlet.java
@@ -7,6 +7,10 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
 
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.UserMarker;
+import com.google.gson.Gson;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
