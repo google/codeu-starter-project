edf97573c5398886dd801e64d2e378818b9c55e1
Mario Verdugo <mv7757@email.arizona.edu>
2019-03-31 22:32:01 -0700

    added in a second parameter "redirect" in the fetch so that the messageServlet can be used more universally. Redirect is where you shouldredirect when you send a message and that is for now the page you sent the message from

--- src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
+++ src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
@@ -17,8 +17,9 @@ public class ImageUploadUrlServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
   	String user = request.getParameter("recipient");
+  	String redirectLink = request.getParameter("redirect");
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/messages?recipient=" + user);
+    String uploadUrl = blobstoreService.createUploadUrl("/messages?recipient=" + user + "&redirect=" + redirectLink);
 
     response.setContentType("text/html");
     response.getOutputStream().println(uploadUrl);
