b7386dda2373f3c79d842955359c9ed0bac51b09
Faesal Murad <faesalm@uvic.ca>
2019-04-02 09:18:40 -0700

    added item class

---
+++ src/main/java/com/google/codeu/data/Item.java
@@ -0,0 +1,34 @@
+package com.google.codeu.data;
+
+public class Item {
+
+  private String title;
+  private String price;
+  private String email;
+  private String description;
+  // TODO: add image support
+  
+  public Item(String title, Double price, String email, String description) {
+    this.title = title;
+    this.price = price;
+    this.email = email;
+    this.description = description;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+  
+  public String getTitle() {
+    return title;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+  
+  public String getDescription() {
+    return description;
+  }
+
+}
