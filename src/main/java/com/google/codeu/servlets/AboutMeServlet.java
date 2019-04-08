2622409810f51b66310cbb36c36d0d156bd2cc48
Michelle Zhang <michelle.zhang@berkeley.edu>
2019-04-07 23:53:27 -0700

    created aboutMeProfile branch for new updates

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.jsoup*
+import org.jsoup.*;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
