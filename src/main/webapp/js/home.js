01b7f6ef97c16c1cd3947e7e7e564566c7bd3dc9
Alyssa Lau <alyssalau@berkeley.edu>
2019-04-12 19:54:00 -0700

    adding functiaonlity to fetch logo upload url

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -2,3 +2,19 @@ var loadFile = function(event) {
   var image = document.getElementById('output');
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+function fetchLogoUploadUrlAndShowForm() {
+  fetch('/logo-detect')
+      .then((response) => {
+        return response.text();
+      })
+      .then((LogoUploadUrlServlet) => {
+        const logo = document.getElementById('logo-form');
+        logo.action = LogoUploadUrlServlet;
+      });
+}
+
+/** Fetches data and populates the UI of the page. */
+function buildUI() {
+  fetchLogoUploadUrlAndShowForm();
+}
