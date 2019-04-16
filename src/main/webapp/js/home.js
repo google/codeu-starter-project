aa38ce5232f5d5881702980193ca2f20826f9e6c
Alyssa Lau <alyssalau@berkeley.edu>
2019-04-16 02:19:49 -0700

    completed displaying an uploaded image after saving to blobstore

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -9,7 +9,23 @@ function fetchLogoUploadUrlAndShowForm() {
       });
 }
 
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function displayImage() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+    if (this.files && this.files[0]) {
+      var img = document.querySelector('img');
+      img.src = URL.createObjectURL(this.files[0]);
+      img.onload = imageIsLoaded;
+    }
+  });
+}
+
 /** Fetches data and populates the UI of the page. */
 function buildUI() {
   fetchLogoUploadUrlAndShowForm();
+  displayImage();
 }
